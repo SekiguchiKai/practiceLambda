fa52ee61019f6cde236f87858a7d1b764469ab12
SekiguchiKai <sekiguchikai@SekiguchiKai-1.local>
2017-01-07 01:35:16 +0900

    JavaDocを更新

--- src/com/company/FizzBuzz.java
+++ src/com/company/FizzBuzz.java
@@ -18,14 +18,12 @@ public class FizzBuzz {
 
         // ラムダ1
         System.out.println("---------FizzBuzz1---------");
-                IntStream.range(1, 101).
+        IntStream.range(1, 101).
                 mapToObj(num -> num % 15 == 0 ? "FizzBuzz!"
                         : num % 3 == 0 ? "Fizz!"
                         : num % 5 == 0 ? "Buzz!"
                         : String.valueOf(num))
                 .forEach(numString -> System.out.println(numString));
-
-
 
 
         // ラムダ2
@@ -35,6 +33,12 @@ public class FizzBuzz {
     }
 
 
+    /**
+     * FizzBuzzを実行するメソッド
+     *
+     * @param num 数字
+     * @return FizzBuzzの審査結果
+     */
     public static String fizzBuzz(int num) {
         return num % 15 == 0 ? "FizzBuzz!" :
                 num % 3 == 0 ? "Fizz!" :
